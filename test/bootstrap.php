2f39135eba2964dde62ae1246bc4eaea1c16bc21
mariojrrc <mariojr.rcosta@gmail.com>
2017-01-27 16:42:32 -0200

    Adicionado AbstractTestCase

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -15,4 +15,16 @@ define('APPLICATION_DATA', TEST_ROOT . '/_files/data');
 /**
  * Setup autoloading
  */
-require __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    $loader = include __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ .'/../../../vendor/autoload.php')) {
+    $loader = include __DIR__ .'/../../../vendor/autoload.php';
+} elseif (file_exists(__DIR__ .'/../../../autoload.php')) {
+    $loader = include __DIR__ .'/../../../autoload.php';
+} else {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
+
+// Carrega os namespaces para teste
+$loader->addPsr4("RealejoTest\\", __DIR__ );
