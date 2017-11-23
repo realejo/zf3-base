759a7ae9729372492f1c1c60436880ae00bdb5f2
Rodrigo Capile <rcapile@gmail.com>
2017-11-23 11:46:26 -0200

    Coloquei o MvcUtil usado no zf3-admin

---
+++ src/MvcUtils/LazyControllerAbstractFactory.php
@@ -0,0 +1,19 @@
+<?php
+namespace Realejo\MvcUtils;
+
+use Interop\Container\ContainerInterface;
+use Zend\Mvc\Controller\LazyControllerAbstractFactory as ZendLazyControllerAbstractFactory;
+
+class LazyControllerAbstractFactory extends ZendLazyControllerAbstractFactory
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $invoke = parent::__invoke($container, $requestedName, $options);
+
+        if (method_exists($invoke, 'setServiceLocator')) {
+            $invoke->setServiceLocator($container);
+        }
+
+        return $invoke;
+    }
+}
---
+++ src/MvcUtils/ServiceLocatorTrait.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Realejo\MvcUtils;
+
+use Zend\ServiceManager\ServiceManager;
+
+trait ServiceLocatorTrait
+{
+
+    /**
+     * @var ServiceManager
+     */
+    public $serviceLocator;
+
+    /**
+     * @return ServiceManager
+     */
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
+
+    /**
+     * @param ServiceManager $serviceLocator
+     * @return self
+     */
+    public function setServiceLocator(ServiceManager $serviceLocator)
+    {
+        $this->serviceLocator = $serviceLocator;
+
+        return $this;
+    }
+
+    public function hasServiceLocator()
+    {
+        return null !== $this->serviceLocator;
+    }
+
+    public function getFromServiceLocator($class)
+    {
+        if (! $this->hasServiceLocator()) {
+            return new ServiceManager();
+        }
+
+        if (! $this->getServiceLocator()->has($class) && $this->getServiceLocator() instanceof ServiceManager) {
+            $newService = new $class();
+            if (method_exists($newService, 'setServiceLocator')) {
+                $newService->setServiceLocator($this->getServiceLocator());
+            }
+            $this->getServiceLocator()->setService($class, $newService);
+        }
+
+        return $this->getServiceLocator()->get($class);
+    }
+}
