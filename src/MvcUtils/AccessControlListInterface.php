0dc3c8dce9a1d6710dd56491f6127c681eacc756
mariojrrc <mariojr.rcosta@gmail.com>
2017-12-07 15:32:08 -0200

    colocado a interface do acl que será utiliizado nos Module.php

---
+++ src/MvcUtils/AccessControlListInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Realejo\MvcUtils;
+/**
+ * Describes the interface of a module that exposes access control list to admin.
+ */
+interface AccessControlListInterface
+{
+    /**
+     * Retuns an arrays contains two items. First, the \Zend\Permissions\Acl\Acl object containing the definitions of
+     * roles and resources. Second, an array containing roles descriptions.
+     *
+     * @return array[\Zend\Permissions\Acl\Acl, array]
+     */
+    public function getAcl();
+}
