e5f9e0c49de23c8c72ba216af1b6801432687710
Rodrigo Capilé <rcapile@gmail.com>
2018-05-10 18:16:01 -0300

    deadcode

--- src/Module.php
+++ src/Module.php
@@ -2,14 +2,6 @@
 
 namespace Realejo;
 
-use Realejo\MvcUtils\AccessControlListInterface;
-use RealejoAdmin\Model\Permissions\Acl;
-use Zend\Console\Request as ConsoleRequest;
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-use Zend\Router\RouteMatch;
-use Zend\Session;
-
 class Module
 {
     public function getConfig()
